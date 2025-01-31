357129323f0e9cb577cf44b6e6a057a87e1c29b9
Doeunnkimm <doeunnkimm@gmail.com>
2025-02-01 02:06:06 +0900

    fix: font

--- src/pages/main/page.tsx
+++ src/pages/main/page.tsx
@@ -49,7 +49,7 @@ export const MainPage = () => {
         />
       </div>
       <div css={contentCss}>
-        <GradientWaveText style={{ fontSize: '80px', fontWeight: 'bold' }}>Cloud</GradientWaveText>
+        <GradientWaveText style={{ fontSize: '30px' }}>Cloud !</GradientWaveText>
       </div>
     </div>
   );
